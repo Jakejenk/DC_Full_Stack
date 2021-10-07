4353530766bc6b75510a1acfc971a9025b9b2a76
Charles Eldridge <charleseldridge@Charless-MacBook-Pro.local>
2021-10-07 12:54:12 -0400

    further advances on get functions

--- templates/home.js
+++ templates/home.js
@@ -59,26 +59,65 @@ function loadRideData() {
     });
 }
 
-function loadDistanceData(data) {
-    let rideData = data['rides'];
-    let length = rideData.length;
-    console.log(rideData);
+function loadTotalDistance() {
+
+    fetch("http://localhost:3300/rides")
+    .then(res => res.json())
+    .then(data => {
+        let rideData = data;
+        let length = rideData.length;
+        console.log(rideData);
     
-    for (let i = 0 ; i < length ; i++) {
-        let displayDiv = document.getElementById("displayDiv");
-        let oneDistance = rideData[i]['distance'];
-        let totalDistance = oneDistance
+        for (let i = 0 ; i < length ; i++) {
+            
+            // let oneDistance = rideData[i]['distance'];
+            const row3Div = document.getElementById("distanceDiv");
+
+            let distanceDiv = document.createElement("div");
+            distanceDiv.innerHTML = rideData[i]['distance'];
+            row3Div.append(distanceDiv);
+        }
 
 
-    };
+    })
 }
 
 function dateFunction() { // Capture form parameters for date of ride
-    let dateValue = document.getElementById(rideDate).value;
-    let url = ("/rides/:" + dateValue);
+    let dateValue = document.getElementById("rideDate").value;
+    let url = "/rides/" + dateValue;
     console.log(dateValue);
     console.log(url);
     fetch(url)
     .then(res => res.json())
     .then(data => console.log(data))
+
+        let soloRide = data;
+
+        const rowDiv = document.getElementById("nameDiv");
+        const row2Div = document.getElementById("dateDiv");
+        const row3Div = document.getElementById("distanceDiv");
+        const row4Div = document.getElementById("locationDiv");
+        const row5Div = document.getElementById("difficultyDiv");
+        
+        let nameDiv = document.createElement("div");
+        nameDiv.innerHTML = soloData[i]['user_name'];
+        //console.log(orgAPI[i]['user_name']);
+        rowDiv.append(nameDiv);
+
+        let dateDiv = document.createElement("div");
+        dateDiv.innerHTML = soloData[i]['date_of_ride'];
+        row2Div.append(dateDiv);
+
+        let distanceDiv = document.createElement("div");
+        distanceDiv.innerHTML = soloData[i]['distance'];
+        row3Div.append(distanceDiv);
+
+        let locationDiv = document.createElement("div");
+        locationDiv.innerHTML = soloData[i]['location_of_ride'];
+        row4Div.append(locationDiv);
+
+        let difficultyDiv = document.createElement("div");
+        difficultyDiv.innerHTML = rideData[i]['difficulty_level'];
+        row5Div.append(difficultyDiv);
+
 }
