e59c31fd1979d47c634f70f49a02f3c6b90487d8
Jake Jenkins <jake.jeknins9510@gmail.com>
2021-10-12 22:01:19 -0400

    pushing registration validation

--- templates/userloginregister.js
+++ templates/userloginregister.js
@@ -57,6 +57,43 @@ function validateLogin() {
   } else {
     loginUser();
   }
+
+  function validateRegistration() {
+    if (document.myForm.user_name.value == "") {
+      alert("Please provide your username!");
+      document.myForm.user_name.focus();
+      return false;
+    }
+    if (document.myForm.first_name.value == "") {
+        alert("Please provide your First Name!");
+        document.myForm.first_name.focus();
+        return false;
+      }
+      if (document.myForm.last_name.value == "") {
+        alert("Please provide your Last Name!");
+        document.myForm.last_name.focus();
+        return false;
+      }
+    if (document.myForm.skill_level.value == "") {
+      alert("Please provide your Skill Level!");
+      document.myForm.skill_level.focus();
+      return false;
+    }
+    if (document.myForm.password.value == "") {
+        alert("Please provide your Password!");
+        document.myForm.password.focus();
+        return false;
+      }
+      if (document.myForm.email.value == "") {
+        alert("Please provide your Email!");
+        document.myForm.email.focus();
+        return false;
+      }
+      else {
+        registerUser();
+      }
+}
+
   //   if (document.myForm.Zip.value == "" || isNaN(document.myForm.Zip.value) ||
   //     document.myForm.Zip.value.length != 5) {
 
