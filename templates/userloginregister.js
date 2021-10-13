74e6b3652403e46708fbc8eb3890e4b73b19845f
Jake Jenkins <jake.jeknins9510@gmail.com>
2021-10-12 23:26:30 -0400

    Registration validation working, succesfully registering.

--- templates/userloginregister.js
+++ templates/userloginregister.js
@@ -2,36 +2,49 @@
 
 function registerUser() {
   fetch("http://localhost:3000/registrationAttempt", {
-      method: "POST",
-      headers: {
-        // Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        user_name: document.getElementById("user_name").value,
-        first_name: document.getElementById("first_name").value,
-        last_name: document.getElementById("last_name").value,
-        email: document.getElementById("email").value,
-        password: document.getElementById("password").value,
-        skill_level: document.getElementById("skill_level").value,
-      }),
-    })
-    .then((res) => res.json())
-    .then((res) => (res));
-}
+    method: "POST",
+    headers: {
+      // Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      user_name: document.getElementById("user_name").value,
+      first_name: document.getElementById("first_name").value,
+      last_name: document.getElementById("last_name").value,
+      skill_level: document.getElementById("skill_level").value,
+      password: document.getElementById("password").value,
+      email: document.getElementById("email").value
+    }),
+  })
+    .then(res => (res.json()))
+    .then(res => {
+      window.location.replace("http://localhost:3000/home");
+      const userName = document.getElementById("user_name").value;
+      sessionStorage.setItem("UserName", userName);
+    }
+    //   if (res.isMatch === "false") {
+    //     alert("The email & password combination is incorrect. Please try again.");
+    //   } else if (res.isMatch === "true") {
+    //     sessionStorage.setItem("UserName", userName);
+    //     window.location.replace("http://localhost:3000/");
+    //   }
+
+    // })
+    )}
+
 
 function loginUser() {
   fetch("http://localhost:3000/loginAttempt", {
-      method: "POST",
-      headers: {
-        // "Accept": "text/html",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        user_name: document.getElementById("user_name").value,
-        password: document.getElementById("password").value,
-      })
-    }).then(res => (res.json()))
+    method: "POST",
+    headers: {
+      // "Accept": "text/html",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      user_name: document.getElementById("user_name").value,
+      password: document.getElementById("password").value,
+    })
+  }).then(res => (res.json()))
     .then(res => {
       if (res.isMatch === "false") {
         alert("The email & password combination is incorrect. Please try again.");
@@ -58,41 +71,6 @@ function validateLogin() {
     loginUser();
   }
 
-  function validateRegistration() {
-    if (document.myForm.user_name.value == "") {
-      alert("Please provide your username!");
-      document.myForm.user_name.focus();
-      return false;
-    }
-    if (document.myForm.first_name.value == "") {
-        alert("Please provide your First Name!");
-        document.myForm.first_name.focus();
-        return false;
-      }
-      if (document.myForm.last_name.value == "") {
-        alert("Please provide your Last Name!");
-        document.myForm.last_name.focus();
-        return false;
-      }
-    if (document.myForm.skill_level.value == "") {
-      alert("Please provide your Skill Level!");
-      document.myForm.skill_level.focus();
-      return false;
-    }
-    if (document.myForm.password.value == "") {
-        alert("Please provide your Password!");
-        document.myForm.password.focus();
-        return false;
-      }
-      if (document.myForm.email.value == "") {
-        alert("Please provide your Email!");
-        document.myForm.email.focus();
-        return false;
-      }
-      else {
-        registerUser();
-      }
-}
 
   //   if (document.myForm.Zip.value == "" || isNaN(document.myForm.Zip.value) ||
   //     document.myForm.Zip.value.length != 5) {
@@ -108,7 +86,41 @@ function validateLogin() {
   //   return (true);
 }
 
-
+function validateRegistration() {
+  if (document.getElementById("user_name").value == "") {
+    alert("Please provide your username!");
+    document.getElementById("user_name").focus();
+    return false;
+  }
+  if (document.getElementById("first_name").value == "") {
+    alert("Please provide your username!");
+    document.getElementById("first_name").focus();
+    return false;
+  }
+  if (document.getElementById("last_name").value == "") {
+    alert("Please provide your username!");
+    document.getElementById("last_name").focus();
+    return false;
+  }
+  if (document.getElementById("skill_level").value == "") {
+    alert("Please provide your username!");
+    document.getElementById("skill_level").focus();
+    return false;
+  }
+  if (document.getElementById("password").value == "") {
+    alert("Please provide your username!");
+    document.getElementById("password").focus();
+    return false;
+  }
+  if (document.getElementById("email").value == "") {
+    alert("Please provide your username!");
+    document.getElementById("email").focus();
+    return false;
+  }
+  else {
+    registerUser();
+  }
+}
 
 function deleteRide() {
   // e.preventDefault();
@@ -119,16 +131,16 @@ function deleteRide() {
   // console.log(url);
   // fetch(url)
   fetch("http://localhost:3000/deleteRide", {
-      method: "DELETE",
-      headers: {
-        // Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        location_of_ride: document.getElementById("location").value,
-        user_name: document.getElementById("rider").value,
-      }),
-    })
+    method: "DELETE",
+    headers: {
+      // Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      location_of_ride: document.getElementById("location").value,
+      user_name: document.getElementById("rider").value,
+    }),
+  })
     .then(res => res.json())
     .then(data => console.log(data))
 }
