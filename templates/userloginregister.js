5e813a50c10d5cbca25d2e68c96d030747b694be
Jake Jenkins <jake.jeknins9510@gmail.com>
2021-10-13 21:39:05 -0400

    Pushing user modification page

--- templates/userloginregister.js
+++ templates/userloginregister.js
@@ -130,3 +130,33 @@ function deleteRide() {
     .then(res => res.json())
     .then(data => console.log(data))
 }
+
+function modifyUser() {
+  let user_Name = sessionStorage.getItem("UserName");
+  let url = "user/modify/" + user_Name;
+  if ((user_Name != "No One") && (user_Name != null)) {
+    fetch("http://localhost:3000/" + url, {
+      method: "PUT",
+      headers: {
+        // Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_name: user_Name,
+        first_name: document.getElementById("first_name").value,
+        last_name: document.getElementById("last_name").value,
+        skill_level: document.getElementById("skill_level").value,
+        password: document.getElementById("password").value,
+        email: document.getElementById("email").value
+      }),
+    })
+      .then(res => (res.json()))
+      .then(res => {
+        const userName = document.getElementById("user_name").value;
+        alert("Succesfully Changed Account")
+      })
+  }
+  else {
+    alert("Please Sign in First")
+  }
+}
